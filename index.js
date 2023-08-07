df2c6aeaa4eb03d069fb7e6c2f931b5546307c66
devinreeks <devin@gotillit.com>
2023-08-07 17:57:47 +1000

    spike tested node canvas

---
+++ index.js
@@ -0,0 +1,47 @@
+const { createCanvas, loadImage } = require('canvas')
+const canvas = createCanvas(200, 200)
+const ctx = canvas.getContext('2d')
+const fs = require('fs')
+const path = require('node:path');
+const { Timer } = require('timer-node');
+
+const timer = new Timer({ label: 'test-timer' });
+
+const t = async () => {
+  timer.start();
+  console.log('start')
+// Write "Awesome!"
+  ctx.font = '30px Impact'
+  ctx.rotate(0.1)
+  ctx.fillText('Awesome!', 50, 100)
+
+// Draw line under text
+  var text = ctx.measureText('Awesome!')
+  ctx.strokeStyle = 'rgba(0,0,0,0.5)'
+  ctx.beginPath()
+  ctx.lineTo(50, 102)
+  ctx.lineTo(50 + text.width, 102)
+  ctx.stroke()
+
+  const image = await loadImage('lime-cat.png')
+  ctx.drawImage(image, 50, 0, 70, 70)
+  const image1 = await loadImage('lime-cat.png')
+  ctx.drawImage(image1, 150, 10, 170, 170)
+  const image2 = await loadImage('lime-cat.png')
+  ctx.drawImage(image2, 180, 10, 170, 170)
+  const image3 = await loadImage('lime-cat.png')
+  ctx.drawImage(image3, 50, 10, 170, 170)
+  const image4 = await loadImage('lime-cat.png')
+  ctx.drawImage(image4, 50, 10, 170, 170)
+  const image5 = await loadImage('bris.png')
+  ctx.drawImage(image5, 10, 10, 170, 10)
+
+  canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'test.png')))
+  console.log('end')
+  console.log(timer.ms());
+}
+
+t();
+
+
+
